4f734d1498df36f85d6833f7bc368c5b74db36de
Kaan Tasbas <mktasbas@gmail.com>
2022-05-05 22:39:53 -0400

    feat: upgradeable

--- src/BackedCommunity.sol
+++ src/BackedCommunity.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: APGL-3.0-only
 pragma solidity 0.8.13;
 
-import {ERC721Soulbound} from "./ERC721Soulbound.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {ERC721SoulboundUpgradeable} from "./ERC721SoulboundUpgradeable.sol";
+import {BackedCommunityStorageV1} from "./BackedCommunityStorage.sol";
 import {IRenderer} from "./interfaces/IRenderer.sol";
 
 /// @title Backed Protocol Community NFT
 /// @author 0xMetas
-contract BackedCommunity is ERC721Soulbound {
-    // --------- //
-    // [ state ] //
-    // --------- //
-    address public renderer;
-
+contract BackedCommunity is
+    Initializable,
+    ERC721SoulboundUpgradeable,
+    BackedCommunityStorageV1
+{
     // --------------- //
     // [ constructor ] //
     // --------------- //
-    constructor() ERC721Soulbound("BackedCommunity", "BACKED") {}
+    function initialize() public initializer {
+        __ERC721Soulbound_init("BackedCommunity", "BACKED");
+    }
 
     // ------------ //
     // [ metadata ] //
--- src/ERC721SoulboundUpgradeable.sol
+++ src/ERC721SoulboundUpgradeable.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: APGL-3.0-only
 pragma solidity 0.8.13;
 
-import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {ERC721Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC721/ERC721Upgradeable.sol";
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
 /// @title An ERC721 extension for soulbound non-fungible tokens
 /// @author 0xMetas
 /// @dev This extension limits the balance of each account to a maximum of 1,
 /// and prevents transfers of the NFT (except for the contract owner)
-contract ERC721Soulbound is ERC721, Ownable {
+contract ERC721SoulboundUpgradeable is
+    Initializable,
+    ERC721Upgradeable,
+    OwnableUpgradeable
+{
     // --------------- //
     // [ constructor ] //
     // --------------- //
@@ -16,9 +21,12 @@ contract ERC721Soulbound is ERC721, Ownable {
     /// @dev Initializes the base ERC721 contract
     /// @param name_ The name of the ERC721 token
     /// @param symbol_ The symbol of the ERC721 token
-    constructor(string memory name_, string memory symbol_)
-        ERC721(name_, symbol_)
-    {}
+    function __ERC721Soulbound_init(string memory name_, string memory symbol_)
+        internal
+        onlyInitializing
+    {
+        __ERC721_init(name_, symbol_);
+    }
 
     // ------------- //
     // [ soulbound ] //
